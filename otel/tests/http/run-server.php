c018abe93fe98dd76b4d54e4ab6eaded47bea3c9
Brett McBride <brett@deakin.edu.au>
2025-02-27 21:32:23 +1100

    restore deleted test files

---
+++ otel/tests/http/run-server.php
@@ -0,0 +1,42 @@
+<?php
+
+function run_server(string $file, array $options): void {
+    $host = '127.0.0.1';
+    $port = 8080;
+    $docRoot = __DIR__;
+    $router = $docRoot . '/' . $file;
+    $server_log = sprintf('/tmp/server-%s.log', uniqid());
+    $response_log = sprintf('/tmp/response-%s.log', uniqid());
+
+    // Start the PHP built-in web server
+    $cmd = sprintf(
+        '%s php %s -S %s:%d -t %s %s > %s 2>&1 & echo $!',
+        'OTEL_TRACES_EXPORTER=console OTEL_SPAN_PROCESSOR=simple',
+        '-d extension=otel.so',
+        $host,
+        $port,
+        escapeshellarg($docRoot),
+        escapeshellarg($router),
+        $server_log
+    );
+    //echo $cmd;
+    $pid = shell_exec($cmd);
+    usleep(500000); // Wait for server to start
+
+    // Make an HTTP request to the server
+    $url = "http://$host:$port/";
+
+    // Create context with options
+    $context = stream_context_create($options);
+    $response = file_get_contents($url, false, $context);
+
+    // Kill the server
+    exec("kill $pid");
+
+    // Output response
+    echo "==== Response ====\n";
+    echo $response;
+
+    echo "==== Server Output ====\n";
+    echo file_get_contents($server_log);
+}
