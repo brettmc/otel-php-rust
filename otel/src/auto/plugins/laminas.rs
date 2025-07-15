14ed9eb022340477f083b259017cfd3ccb557075
Brett McBride <brett@deakin.edu.au>
2025-07-15 14:55:52 +1000

    controller and action

--- otel/src/auto/plugins/laminas.rs
+++ otel/src/auto/plugins/laminas.rs
@@ -94,17 +94,35 @@ impl LaminasRouteHandler {
         let exec_data_ref = &mut *exec_data;
         let route_match_zval: &mut ZVal = exec_data_ref.get_mut_parameter(0);
         let request = get_request_details();
-        if let Some(route_name_str) = route_match_zval
-            .as_mut_z_obj()
-            .and_then(|obj| obj.call("getMatchedRouteName", []).ok())
-            .and_then(|zv| zv.as_z_str().and_then(|s| s.to_str().ok().map(|s| s.to_owned())))
-        {
-            let name = format!("{} {}", request.method.as_deref().unwrap_or("GET"), route_name_str);
-            tracing::debug!("Auto::Laminas::updateName (MvcEvent::setRouteMatch)");
-            ctx.span().update_name(name);
-            ctx.span().set_attribute(KeyValue::new("php.mvc.framework", "laminas"));
-            //ctx.span().set_attribute(KeyValue::new("php.mvc.controller", "controller-name"));
-            //ctx.span().set_attribute(KeyValue::new("php.mvc.action", "action-name"));
+
+        if let Some(route_match_obj) = route_match_zval.as_mut_z_obj() {
+            let route_name = route_match_obj
+                .call("getMatchedRouteName", [])
+                .ok()
+                .and_then(|zv| zv.as_z_str().and_then(|s| s.to_str().ok().map(|s| s.to_owned())));
+
+            let action = route_match_obj
+                .call("getParam", &mut [ZVal::from("action")])
+                .ok()
+                .and_then(|zv| zv.as_z_str().and_then(|s| s.to_str().ok().map(|s| s.to_owned())));
+
+            let controller = route_match_obj
+                .call("getParam", &mut [ZVal::from("controller")])
+                .ok()
+                .and_then(|zv| zv.as_z_str().and_then(|s| s.to_str().ok().map(|s| s.to_owned())));
+
+            if let Some(route_name_str) = &route_name {
+                let name = format!("{} {}", request.method.as_deref().unwrap_or("GET"), route_name_str);
+                tracing::debug!("Auto::Laminas::updateName (MvcEvent::setRouteMatch)");
+                ctx.span().update_name(name);
+                ctx.span().set_attribute(KeyValue::new("php.http.framework.name", "laminas"));
+                if let Some(controller) = &controller {
+                    ctx.span().set_attribute(KeyValue::new("php.http.framework.controller.name", controller.clone()));
+                }
+                if let Some(action) = &action {
+                    ctx.span().set_attribute(KeyValue::new("php.http.framework.action.name", action.clone()));
+                }
+            }
         }
     }
 }
